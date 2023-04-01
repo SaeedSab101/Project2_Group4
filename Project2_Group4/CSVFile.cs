36dfc9c1bd682b3bfedfc6ee46720263e015ba39
foreandr <foreandr@gmail.com>
2023-04-01 10:01:45 -0400

    Add project files.

---
+++ Project2_Group4/CSVFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2_Group4
+{
+    public class CSVFile
+    {
+        public List<T> CSVDeserialize<T>(string filePath)
+        {
+            var result = new List<T>();
+
+            using (var reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var values = line.Split(',');
+                    var obj = Activator.CreateInstance<T>();
+
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        var propertyInfo = typeof(T).GetProperty($"Column{i + 1}");
+                        if (propertyInfo != null)
+                        {
+                            var convertedValue = Convert.ChangeType(values[i], propertyInfo.PropertyType);
+                            propertyInfo.SetValue(obj, convertedValue);
+                        }
+                    }
+
+                    result.Add(obj);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Project2_Group4/XMLExtension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Project2_Group4
+{
+    public static class XMLExtension
+    {
+        public static void WriteStartDocument(this XmlWriter writeStart, string encoding)
+        {
+            encoding = "UTF-8";
+            writeStart.WriteStartDocument();
+            writeStart.WriteProcessingInstruction("xml", $"version=\"1.0\" encoding=\"{encoding}\"");
+        }
+
+        public static void WriteStartRootElement(this XmlWriter writeStartRoot, string rootElementName)
+        {
+            writeStartRoot.WriteStartElement(rootElementName);
+        }
+
+        public static void WriteEndRootElement(this XmlWriter writeEndRoot)
+        {
+            writeEndRoot.WriteEndElement();
+        }
+
+        public static void WriteStartElement(this XmlWriter writeStartEle, string elementName)
+        {
+            writeStartEle.WriteStartElement(elementName);
+        }
+
+        public static void WriteEndElement(this XmlWriter writeEndEle)
+        {
+            writeEndEle.WriteEndElement();
+        }
+
+        public static void WriteAttribute(this XmlWriter writeAttr, string attributeName, string attributeValue)
+        {
+            writeAttr.WriteAttributeString(attributeName, attributeValue);
+        }
+    }
+}
